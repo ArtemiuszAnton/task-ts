49d1e2487c14684d4b7e0886be66c2e450d2bcc8
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-03-11 14:24:31 +0100

    add 3,4,5 unit test

--- 070.TS Unit Tests/task4.test.ts
+++ 070.TS Unit Tests/task4.test.ts
@@ -2,6 +2,28 @@
 // котором могут быть вложенные массивы, и возвращает новый массив, в котором
 // все элементы являются плоским списком без вложенности.
 
-function flattenArray(arr: any[]): any[] {
 
+const flattenArray = (array: any[] | any): any[] => {
+    try {
+        if (!Array.isArray(array)) throw new Error('This is not array');
+        return array.flat(Infinity)
+    } catch (er: any) {
+        return er.message
+    }
 }
+describe('test for flattenArray', () => {
+    test('correct flatting', () => {
+        const arr = [1, 23, 4, [], [2, 4], [33, [3, 43]]];
+        const arr2 = [1, 2, [1, 2], [1, 2]];
+        const arrFlat = flattenArray(arr);
+        const arrFlat2 = flattenArray(arr2);
+        expect(arrFlat).toEqual([1, 23, 4, 2, 4, 33, 3, 43]);
+        expect(arrFlat2).toEqual([1, 2, 1, 2, 1, 2])
+    })
+
+    test('failed flatting', () => {
+        const objArr = { arr: 2 };
+        const objFlat = flattenArray(objArr);
+        expect(objFlat).toBe('This is not array')
+    })
+})
--- 070.TS Unit Tests/task5.test.ts
+++ 070.TS Unit Tests/task5.test.ts
@@ -1,3 +1,23 @@
+// Напишите функцию chunkArray(array: any[], size: number): any[][], которая
+// принимает массив и число size, и возвращает новый массив, разделенный на
+// подмассивы указанного размера.
+
+
 function chunkArray(array: any[], size: number): any[][] {
-    
+    try {
+        let newArr: any[] = []
+
+        if (size === 0) throw new Error('size can not be 0');
+        if (!Array.isArray(array)) throw new Error('it is not array');
+        if (!array.length) throw new Error('array is empty');
+
+        for (let i = 0; i < array.length; i++) {
+            newArr.push(array.slice(i, i + size))
+        }
+        return newArr
+    } catch (er) {
+        return er.message
+    }
+
 }
+
